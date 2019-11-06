4470f99699c4bec7bfd48da3fe316f5b80c71e23
amlane <twelvexstring@gmail.com>
2019-11-05 21:19:33 -0600

    initial push, basic components created

---
+++ src/components/BookList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../index.css';
+
+function BookList() {
+    return (
+        <div className="main">
+            <ul id="book-list">
+                <li>Book name</li>
+            </ul>
+        </div>
+    );
+}
+
+export default BookList;
